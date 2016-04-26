71e7951831e949347ab08dccb434aec6a8337644
gold bird <pouria346h@gmail.com>
2016-04-26 22:40:22 +0430

    Added files via upload

---
+++ plugins/chat.lua
@@ -0,0 +1,29 @@
+local function run(msg)
+	if msg.text == "pouria" then
+		return "my father"
+	end
+	if msg.text == "amir" then
+		return "my father"
+	end
+	if msg.text == "ainaz" then
+		return "my mother"
+	end
+	if msg.text == "fuck" then
+		return "for you"
+	end
+	if msg.text == "bk" then
+		return "heh"
+	end
+	if msg.text == "salam" then
+		return "Hi welcome to group"
+	end
+	if msg.text == "help" then
+		return "oskol ba alamat"
+	end
+	if msg.text == "346" then 
+		return "is the best"
+	end
+	if msg.text == "amir jon" then 
+		return "my brother"
+	end
+end
